7e37c783f034ff19ad2638deaf1a0d7ed243a25c
RudnytskyiBohdan <rudnytskyi.bohdan@gmail.com>
2022-04-05 21:33:16 +0300

    v0.0.2

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
-#/bin/sh
-
-if [[ssl_port -it 0]] && [[ssl_certificate!=""]] && [[ssl_certificate_key!=""]] then
-         sed '98,115 s/^#//g' /etc/nginx/nginx.conf
-         return 0
-fi
+#/bin/bash
+  
+  if ["$ssl_certificate"=""] && ["$ssl_certificate_key"=""] ; then
+      exit
+  else
+      sed '/HTTPS server/,$ s/#//g; /HTTPS server/c # HTTPS server' /etc/nginx/nginx.conf
+  fi       
+  exit
